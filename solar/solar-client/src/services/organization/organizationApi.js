c8951a4bdd9cc84f62dc5fedd5ef00d7ed750217
Alis Patel <alispatel123098@gmail.com>
2026-05-21 14:11:57 +0530

    optimized

--- solar/solar-client/src/services/organization/organizationApi.js
+++
@@ -1,7 +0,0 @@
-import api from '../../api/axios';
-
-export const organizationApi = {
-    getChartData: (params) => api.get('/organization/chart-data', { params }),
-    getEmployees: (params) => api.get('/organization/employees', { params }),
-    getStats: () => api.get('/organization/stats'),
-};
